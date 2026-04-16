10a1883506f5e66bfcb0c488e9b166fc65d045f3
Souluigi <marcos2003mlr@gmail.com>
2026-04-16 10:43:05 -0300

    feat: cria a entidade Resenha

---
+++ src/main/java/ifgoiano/gestor_leitura_api/model/Resenha.java
@@ -0,0 +1,49 @@
+package ifgoiano.gestor_leitura_api.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "resenha")
+public class Resenha implements Serializable {
+
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String texto;
+
+    @Column(nullable = false)
+    private float avaliacao;
+
+    @ManyToOne
+    @JoinColumn(name = "leitor_id")
+    private Leitor leitor;
+
+    @ManyToOne
+    @JoinColumn(name = "livro_id")
+    private Livro livro;
+
+    public Resenha(){
+
+    }
+
+    public Resenha(Long id, String texto, float avaliacao, Leitor leitor, Livro livro) {
+        this.id = id;
+        this.texto = texto;
+        this.avaliacao = avaliacao;
+        this.leitor = leitor;
+        this.livro = livro;
+    }
+}
