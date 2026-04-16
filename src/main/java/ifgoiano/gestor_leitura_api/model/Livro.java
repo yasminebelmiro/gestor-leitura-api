9c36f325274bde1af9bd8ff25b02e40eb3d5db71
Souluigi <marcos2003mlr@gmail.com>
2026-04-16 10:42:20 -0300

    feat: cria a entidade Livro

---
+++ src/main/java/ifgoiano/gestor_leitura_api/model/Livro.java
@@ -0,0 +1,60 @@
+package ifgoiano.gestor_leitura_api.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "livro")
+public class Livro implements Serializable {
+
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String isbn;
+    @Column(nullable = false)
+    private String titulo;
+    @Column(nullable = false)
+    private int numeroPaginas;
+
+
+    //Não sei oq vai aki
+    private List<Autor> autores;
+    //Não sei oq vai aki
+    private List<Genero> generos;
+    //Não sei oq vai aki
+    private Editora editora;
+    //Não sei oq vai aki
+    private List<Resenha> resenhas;
+    //Não sei oq vai aki
+    private Date dataPublicacao;
+
+    public Livro (){
+
+    }
+
+    public Livro(Long id, String isbn, String titulo, int numeroPaginas, List<Autor> autores, List<Genero> generos, Editora editora, List<Resenha> resenhas, Date dataPublicacao) {
+        this.id = id;
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.numeroPaginas = numeroPaginas;
+        this.autores = autores;
+        this.generos = generos;
+        this.editora = editora;
+        this.resenhas = resenhas;
+        this.dataPublicacao = dataPublicacao;
+    }
+}
