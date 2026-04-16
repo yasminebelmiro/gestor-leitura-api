787214c44c72a0bd49c96650b15123b9dbdba56d
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2026-04-16 07:26:07 -0300

    first commit

---
+++ src/main/java/ifgoiano/gestor_leitura_api/GestorLeituraApiApplication.java
@@ -0,0 +1,13 @@
+package ifgoiano.gestor_leitura_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorLeituraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorLeituraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifgoiano/gestor_leitura_api/GestorLeituraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ifgoiano.gestor_leitura_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorLeituraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
