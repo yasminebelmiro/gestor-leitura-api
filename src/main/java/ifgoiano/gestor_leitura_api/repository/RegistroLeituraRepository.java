74d0cd8ce746cd7befeefc05d294d483c6a84669
Darcson Lanucio Ferreira Fernandes <119936543+DarcsonLanucio@users.noreply.github.com>
2026-05-05 12:36:46 -0300

    Refactor Repositoris
    Refatoração repositorys: Leitor, Livro, MetaAnual e RegistroLeitura

--- src/main/java/ifgoiano/gestor_leitura_api/repository/RegistroLeituraRepository.java
+++ src/main/java/ifgoiano/gestor_leitura_api/repository/RegistroLeituraRepository.java
@@ -13,27 +13,33 @@ import java.util.Optional;
 @Repository
 public interface RegistroLeituraRepository extends JpaRepository<RegistroLeitura, Long> {
 
-    // // Lista todos os registros de leitura de um item de estante.
-    // List<RegistroLeitura> findByItemEstanteId(Long itemEstanteId);
+     // Lista todos os registros de leitura de um item de estante.
+     List<RegistroLeitura> findByItemId(Long itemId);
 
-    // // Lista os registros em ordem cronológica.
-    // List<RegistroLeitura> findByItemEstanteIdOrderByDataAsc(Long itemEstanteId);
+     // Lista os registros em ordem cronológica.
+     List<RegistroLeitura> findByItemIdOrderByDataAsc(Long itemId);
 
-    // // Busca o registro mais recente de um item de estante.
-    // Optional<RegistroLeitura> findTopByItemEstanteIdOrderByDataDesc(Long itemEstanteId);
+     // lista os registros em ordem decrescente
+    List<RegistroLeitura> findByItemIdOrderByDataDesc(Long itemId);
 
-    // // Busca registros dentro de um período.
-    // List<RegistroLeitura> findByDataBetween(Date dataInicio, Date dataFim);
+    // Busca o registro mais recente de um item de estante.
+    Optional<RegistroLeitura> findTopByItemIdOrderByDataAsc(Long itemId);
 
-    // // Busca a primeira data registrada para um item.
-    // @Query("SELECT MIN(r.data) FROM RegistroLeitura r WHERE r.itemEstante.id = :itemEstanteId")
-    // Date findPrimeiraDataByItemEstanteId(@Param("itemEstanteId") Long itemEstanteId);
+    // Busca registro mais antigo na estante.
+    Optional<RegistroLeitura> findTopByItemIdOrderByDataDesc(Long itemId);
 
-    // // Busca a última data registrada para um item.
-    // @Query("SELECT MAX(r.data) FROM RegistroLeitura r WHERE r.itemEstante.id = :itemEstanteId")
-    // Date findUltimaDataByItemEstanteId(@Param("itemEstanteId") Long itemEstanteId);
+     // Busca registros dentro de um período.
+     List<RegistroLeitura> findByDataBetween(Date dataInicio, Date dataFim);
 
-    // // Busca a maior página registrada para um item.
-    // @Query("SELECT MAX(r.paginaAtual) FROM RegistroLeitura r WHERE r.itemEstante.id = :itemEstanteId")
-    // Integer findMaiorPaginaAtualByItemEstanteId(@Param("itemEstanteId") Long itemEstanteId);
+     // Busca a primeira data registrada para um item.
+     @Query("SELECT MIN(r.data) FROM RegistroLeitura r WHERE r.item.id = :itemId")
+     Date findPrimeiraDataByItemId(@Param("itemId") Long itemId);
+
+     // Busca a última data registrada para um item.
+     @Query("SELECT MAX(r.data) FROM RegistroLeitura r WHERE r.item.id = :itemId")
+     Date findUltimaDataByItemId(@Param("itemId") Long itemId);
+
+     // Busca a maior página registrada para um item.
+     @Query("SELECT MAX(r.paginaAtual) FROM RegistroLeitura r WHERE r.item.id = :itemId")
+     Integer findMaiorPaginaAtualByItemId(@Param("itemId") Long itemId);
 }
